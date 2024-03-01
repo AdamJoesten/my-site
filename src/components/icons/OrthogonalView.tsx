5fa1ea6ce3f0ebd0815878725bd712491e5d4861
Adam Joesten <adamjoesten@gmail.com>
2024-03-01 17:49:01 -0500

    <bug>: Fix accesibility labels

--- src/components/icons/OrthogonalView.tsx
+++ src/components/icons/OrthogonalView.tsx
@@ -11,6 +11,7 @@ export const OrthogonalView = ({ className = 'stroke-black' }: IconProps) => {
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      aria-label='Orthogonal view'
     >
       <path
         d="M3 21V3H21V21H3Z"
--- src/components/icons/Rocket.tsx
+++ src/components/icons/Rocket.tsx
@@ -11,6 +11,7 @@ export const Rocket = ({ className = 'stroke-black' }: IconProps) => {
       strokeWidth="1.5"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      aria-label='Rocket'
     >
       <path
         d="M16.0614 10.4037L14 17L10 17L7.93865 10.4037C7.35085 8.52273 7.72417 6.47307 8.93738 4.92015L11.5272 1.6052C11.7674 1.29772 12.2326 1.29772 12.4728 1.6052L15.0626 4.92015C16.2758 6.47307 16.6491 8.52273 16.0614 10.4037Z"
